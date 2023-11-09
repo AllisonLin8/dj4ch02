46160e9ee7f18a6a7e0f4e00963ad3f5a9785ddf
AllisonLin8 <doging8@gmail.com>
2023-11-09 21:51:03 +0800

    ch02 completed

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from mysite.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from mysite.models import Post
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+    # posts = Post.objects.all()
+    # post_lists = list()
+    # for count, post in enumerate(posts):
+    #     post_lists.append('No.{}：'.format(str(count)) + str(post) + '<br>')
+    # return HttpResponse(post_lists)
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug = slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
